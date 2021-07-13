a9a6a602ec4f874c1caeaf4c6e0680cdb87a173b
NuengThanaphon01 <78971522+NuengThanaphon01@users.noreply.github.com>
2021-07-13 14:12:07 +0700

    Create Exercise4_Thanaphon_K.py
    Exercise 4

---
+++ Assignment/Exercise4_Thanaphon_K.py
@@ -0,0 +1,10 @@
+FDENG = 80.5
+GNBUS = 75.5
+ICS    = 85.0
+CP     = 90.5
+
+print("----Your Score----")
+print("Foundation English             "," : ",FDENG)
+print("Generall Business              "," : ",GNBUS)
+print("Introdution to Computer Systems"," : ",ICS)
+print("Computer Prohgramming          "," : ",CP )
