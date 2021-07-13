21deb5c89840e8731c3e102402e182ce18fd2c8b
NuengThanaphon01 <78971522+NuengThanaphon01@users.noreply.github.com>
2021-07-13 16:45:18 +0700

    EX5

---
+++ Assignment/Exercise5_1_Thanaphon_K.py
@@ -0,0 +1,6 @@
+a=float(input("Please input x: "))
+b=float(input("Please input y: "))
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print(a,"/",b,"=",a/b)
---
+++ Assignment/Exercise5_2_Thanaphon_K.py
@@ -0,0 +1,4 @@
+s=float(input("Please input distance (km.):"))
+t=float(input("Please input time(hr.):"))
+v=s/t
+print("Speed is ",v,"km/h")
